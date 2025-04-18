6cbdf34ddcffbfb6684d998251010cc0943eb33c
Ian Pan <g-hamu@hotmail.com>
2025-04-18 22:36:41 +0800

    feat: add teller chat block expand icon

--- app/src/main/java/com/onean/momo/ui/tarot_session/TarotSessionScreen.kt
+++ app/src/main/java/com/onean/momo/ui/tarot_session/TarotSessionScreen.kt
@@ -1,5 +1,12 @@
 package com.onean.momo.ui.tarot_session
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.InfiniteRepeatableSpec
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,12 +16,15 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
@@ -27,13 +37,16 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
@@ -47,11 +60,15 @@ import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.onean.momo.R
 import com.onean.momo.ext.SimpleImage
+import com.onean.momo.ext.conditional
 import com.onean.momo.ext.isTablet
+import com.onean.momo.ext.safeClickable
 import com.onean.momo.ui.component.Loading
 import com.onean.momo.ui.component.TarotButton
 import com.onean.momo.ui.component.TipDialog
 import com.onean.momo.ui.draw_card.DrawCardScreen
+import com.onean.momo.ui.theme.Dark30
+import com.onean.momo.ui.theme.White50
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.delay
@@ -154,22 +171,78 @@ fun TarotSessionScreen(
         LaunchedEffect(displayTellerChatLines) {
             displayTellerChatScrollState.animateScrollTo(Int.MAX_VALUE)
         }
-        Text(
+
+        Box(
             modifier = Modifier
                 .align(Alignment.Center)
                 .padding(style.tellerChatPadding)
-                .height(style.tellerChatHeight)
-                .fillMaxWidth()
-                .verticalScroll(displayTellerChatScrollState),
-            text = displayTellerChat,
-            color = Color.White,
-            fontSize = style.tellerChatFontSize,
-            style = TextStyle(lineHeight = 1.2.em),
-            fontWeight = FontWeight.SemiBold,
-            onTextLayout = { result ->
-                displayTellerChatLines = result.lineCount
+        ) {
+            var isExpanded by remember { mutableStateOf(false) }
+
+            Box(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(12.dp))
+                    .background(Dark30)
+                    .padding(8.dp)
+                    .safeClickable {
+                        // click on whole area would expand
+                        isExpanded = !isExpanded
+                    }
+            ) {
+                Text(
+                    modifier = Modifier
+                        .animateContentSize()
+                        .clip(shape = RoundedCornerShape(12.dp))
+                        .conditional(isExpanded) {
+                            padding(top = 100.dp)
+                            fillMaxHeight(0.8F)
+                        }
+                        .conditional(!isExpanded) {
+                            height(style.tellerChatHeight)
+                        }
+                        .fillMaxWidth()
+                        .verticalScroll(displayTellerChatScrollState),
+                    text = displayTellerChat,
+                    color = Color.White,
+                    fontSize = style.tellerChatFontSize,
+                    style = TextStyle(lineHeight = 1.2.em),
+                    fontWeight = FontWeight.SemiBold,
+                    onTextLayout = { result ->
+                        displayTellerChatLines = result.lineCount
+                    }
+                )
+                val isExpandIconVisible = (uiState.step == TarotSessionStep.ReplyQuestion ||
+                    uiState.step is TarotSessionStep.DrawAllKnownCards)
+                if (isExpandIconVisible) {
+                    val expandDownIconDegree by animateFloatAsState(
+                        targetValue = if (isExpanded) 180F else 0F,
+                        animationSpec = tween(800)
+                    )
+                    val infiniteTransition = rememberInfiniteTransition()
+                    val expandDownIconAlpha by infiniteTransition.animateFloat(
+                        initialValue = 0.2F,
+                        targetValue = 1F,
+                        animationSpec = InfiniteRepeatableSpec(
+                            animation = tween(1600),
+                            repeatMode = RepeatMode.Reverse
+                        ),
+                        label = ""
+                    )
+                    SimpleImage(
+                        modifier = Modifier
+                            .padding(top = 8.dp)
+                            .align(Alignment.BottomEnd)
+                            .size(24.dp)
+                            .graphicsLayer {
+                                alpha = expandDownIconAlpha
+                                rotationX = expandDownIconDegree
+                            },
+                        id = R.drawable.expand_down,
+                        tint = White50
+                    )
+                }
             }
-        )
+        }
         Loading(
             modifier = Modifier.align(Alignment.Center),
             isLoading = uiState.loading
@@ -198,14 +271,14 @@ enum class TarotSessionScreenStyle : SessionScreenStyle {
     Phone {
         override val tellerChatFontSize = 22.sp
         override val tellerChatPadding = PaddingValues(start = 20.dp, end = 20.dp, top = 0.dp, bottom = 150.dp)
-        override val tellerChatHeight = 116.dp
+        override val tellerChatHeight = 58.dp
         override val bottomActionBlockPadding = PaddingValues(start = 32.dp, end = 32.dp, top = 0.dp, bottom = 60.dp)
         override val drawCardScreenPadding = PaddingValues(20.dp)
     },
     Tablet {
         override val tellerChatFontSize = 44.sp
         override val tellerChatPadding = PaddingValues(start = 40.dp, end = 40.dp, top = 0.dp, bottom = 300.dp)
-        override val tellerChatHeight = 232.dp
+        override val tellerChatHeight = 116.dp
         override val bottomActionBlockPadding = PaddingValues(start = 64.dp, end = 64.dp, top = 0.dp, bottom = 120.dp)
         override val drawCardScreenPadding = PaddingValues(40.dp)
     },
